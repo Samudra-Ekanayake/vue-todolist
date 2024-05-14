f700115a60e08bf12c190e1b03ef7e4506d3b631
Samudra-Ekanayake <samudra2724@gmail.com>
2024-05-14 17:38:02 +0200

    M2 da completare

--- main.js
+++ main.js
@@ -9,11 +9,17 @@ createApp({
     data() {
         return {
             array: ["Andare in palestra", "fare sport", "Giocare con la figlia", "Cucinare", "Comprare ventilatore"
-
-            ]
+            ],
+            cosaNuova:"",
         }
     },
     methods: {
+        nuovaCosa(){
+            if(this.cosaNuova = "") {
+                this.array.push(this.cosaNuova)
+            }
+            this.cosaNuova = ""
+        }
 
     }
 }).mount('#app')
