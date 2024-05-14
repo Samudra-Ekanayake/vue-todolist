0d2a4a8c324128a7b642a844d5ef61f9575cf495
Samudra-Ekanayake <samudra2724@gmail.com>
2024-05-14 21:15:28 +0200

    M2 completato

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ createApp({
             array: ["Andare in palestra", "fare sport", "Giocare con la figlia", "Cucinare", "Comprare ventilatore"
             ],
             cosaNuova:"",
+            elementoSelezionato: null,
         }
     },
     methods: {
@@ -14,7 +15,18 @@ createApp({
                 this.array.push(this.cosaNuova)
             }
             this.cosaNuova = ""
-        }
+        },
+
+        selezionaCosa(i){
+            this.elementoSelezionato = i
+        },
+
+        rimuoviCosa(i){
+            if(this.elementoSelezionato != null){
+            this.array.splice(i, 1)
+            }
+            this.elementoSelezionato = null
+        },
 
     }
 }).mount('#app')
