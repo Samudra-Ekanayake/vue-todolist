fa04fbb49bb324b5374d4884f62e7015afdf7ec7
Samudra-Ekanayake <samudra2724@gmail.com>
2024-05-14 16:28:57 +0200

    M1

---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+const { createApp } = Vue
+
+createApp({
+    data() {
+        return {
+            array: ["Andare in palestra", "fare sport", "Giocare con la figlia", "Cucinare", "Comprare ventilatore"
+
+            ]
+        }
+    },
+    methods: {
+
+    }
+}).mount('#app')
